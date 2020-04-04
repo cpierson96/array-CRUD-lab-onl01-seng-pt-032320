da448d3fb8f09c8e2d754d4bb461bc19756cf4d8
Claudia Pierson <c172@umbc.edu>
2020-04-04 04:08:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  << ["Xavier", "Will", "Claudia", "Brownie"]
+   .push["Xavier", "Will", "Claudia", "Brownie"]
 end
 
 def add_element_to_end_of_array(array, element)
